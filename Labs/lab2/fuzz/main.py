7bfdc851a8fc66da82c83b866eb34d640d1d671c
makezh <max.kezhaev@gmail.com>
2023-10-14 14:02:06 +0300

    changing paths

--- Labs/lab2/fuzz/main.py
+++ Labs/lab2/fuzz/main.py
@@ -5,10 +5,10 @@ from functions import *
 
 def main():
     # = CHECK ARGUMENTS = #
-    input_file = sys.argv[1] if len(sys.argv) > 1 else "../files/input.txt"
-    output_file = sys.argv[2] if len(sys.argv) > 2 else "../files/output.txt"
-    json_file = sys.argv[3] if len(sys.argv) > 3 else "../files/result.json"
-    result_file = sys.argv[4] if len(sys.argv) > 4 else "../files/RESULT.txt"
+    input_file = f"../{sys.argv[1]}" if len(sys.argv) > 1 else "../files/input.txt"
+    output_file = f"../{sys.argv[2]}" if len(sys.argv) > 2 else "../files/output.txt"
+    json_file = f"../{sys.argv[3]}" if len(sys.argv) > 3 else "../files/result.json"
+    result_file = f"../{sys.argv[4]}" if len(sys.argv) > 4 else "../files/RESULT.txt"
     # = = = = = = = = = = #
 
     # = CONSTANTS = #
