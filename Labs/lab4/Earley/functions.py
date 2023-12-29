18554cfd1bcd209a4f486ea84a197ab48c19b4ba
makezh <max.kezhaev@gmail.com>
2023-12-29 03:39:21 +0300

    adding State of Earley

--- Labs/lab4/Earley/functions.py
+++ Labs/lab4/Earley/functions.py
@@ -2,109 +2,173 @@ from collections import defaultdict
 
 
 class Rule(object):
-	"""
-	Represents a CFG rule.
-	"""
+    """
+    Represents a CFG rule.
+    """
 
-	def __init__(self, lhs, rhs):
-		"""
-		Represents the rule 'lhs -> rhs', where lhs is a non-terminal and
-		rhs is a list of non-terminals and terminals.
-		"""
-		self.lhs, self.rhs = lhs, rhs
+    def __init__(self, lhs, rhs):
+        """
+        Represents the rule 'lhs -> rhs', where lhs is a non-terminal and
+        rhs is a list of non-terminals and terminals.
+        """
+        self.lhs, self.rhs = lhs, rhs
 
-	def __contains__(self, sym):
-		return sym in self.rhs
+    def __contains__(self, sym):
+        return sym in self.rhs
 
-	def __eq__(self, other):
-		if type(other) is Rule:
-			return self.lhs == other.lhs and self.rhs == other.rhs
+    def __eq__(self, other):
+        if type(other) is Rule:
+            return self.lhs == other.lhs and self.rhs == other.rhs
 
-		return False
+        return False
 
-	def __getitem__(self, i):
-		return self.rhs[i]
+    def __getitem__(self, i):
+        return self.rhs[i]
 
-	def __len__(self):
-		return len(self.rhs)
+    def __len__(self):
+        return len(self.rhs)
 
-	def __repr__(self):
-		return self.__str__()
+    def __repr__(self):
+        return self.__str__()
 
-	def __str__(self):
-		return self.lhs + ' -> ' + ' '.join(self.rhs)
+    def __str__(self):
+        return self.lhs + ' -> ' + ' '.join(self.rhs)
 
 
 class Grammar(object):
-	"""
-	Represents a CFG.
-	"""
+    """
+    Represents a CFG.
+    """
 
-	def __init__(self):
-		# The rules are represented as a dictionary from L.H.S to R.H.S.
-		self.rules = defaultdict(list)
+    def __init__(self):
+        # The rules are represented as a dictionary from L.H.S to R.H.S.
+        self.rules = defaultdict(list)
 
-	def add(self, rule):
-		"""
-		Adds the given rule to the grammar.
-		"""
+    def add(self, rule):
+        """
+        Adds the given rule to the grammar.
+        """
 
-		self.rules[rule.lhs].append(rule)
+        self.rules[rule.lhs].append(rule)
 
-	@staticmethod
-	def load_grammar(fpath):
-		"""
-		Loads the grammar from file
-		"""
+    @staticmethod
+    def load_grammar(fpath):
+        """
+        Loads the grammar from file
+        """
 
-		grammar = Grammar()
+        grammar = Grammar()
 
-		with open(fpath) as f:
-			for line in f:
-				line = line.strip()
+        with open(fpath) as f:
+            for line in f:
+                line = line.strip()
 
-				if len(line) == 0:
-					continue
+                if len(line) == 0:
+                    continue
 
-				entries = line.split('->')
-				lhs = entries[0].strip()
-				for rhs in entries[1].split('|'):
-					grammar.add(Rule(lhs, rhs.strip().split()))
+                entries = line.split('->')
+                lhs = entries[0].strip()
+                for rhs in entries[1].split('|'):
+                    grammar.add(Rule(lhs, rhs.strip().split()))
 
-		return grammar
+        return grammar
 
-	def __repr__(self):
-		return self.__str__()
+    def __repr__(self):
+        return self.__str__()
 
-	def __str__(self):
-		s = [str(r) for r in self.rules['S']]
+    def __str__(self):
+        s = [str(r) for r in self.rules['S']]
 
-		for nt, rule_list in self.rules.items():
-			if nt == 'S':
-				continue
+        for nt, rule_list in self.rules.items():
+            if nt == 'S':
+                continue
 
-			s += [str(r) for r in rule_list]
+            s += [str(r) for r in rule_list]
 
-		return '\n'.join(s)
+        return '\n'.join(s)
 
-	# Returns the rules for a given Non-terminal.
-	def __getitem__(self, nt):
-		return self.rules[nt]
+    # Returns the rules for a given Non-terminal.
+    def __getitem__(self, nt):
+        return self.rules[nt]
 
-	def is_terminal(self, sym):
-		"""
-		Checks is the given symbol is terminal.
-		"""
+    def is_terminal(self, sym):
+        """
+        Checks is the given symbol is terminal.
+        """
 
-		return len(self.rules[sym]) == 0
+        return len(self.rules[sym]) == 0
 
-	def is_tag(self, sym):
-		"""
-		Checks whether the given symbol is a tag, i.e. a non-terminal with rules
-		to solely terminals.
-		"""
+    def is_tag(self, sym):
+        """
+        Checks whether the given symbol is a tag, i.e. a non-terminal with rules
+        to solely terminals.
+        """
 
-		if not self.is_terminal(sym):
-			return all(self.is_terminal(s) for r in self.rules[sym] for s in r.rhs)
+        if not self.is_terminal(sym):
+            return all(self.is_terminal(s) for r in self.rules[sym] for s in r.rhs)
 
-		return False
+        return False
+
+
+class EarleyState(object):
+    """
+    Represents a state in the Earley algorithm.
+    """
+
+    GAM = '<GAM>'
+
+    def __init__(self, rule, dot=0, sent_pos=0, chart_pos=0, back_pointers=None):
+        if back_pointers is None:
+            back_pointers = []
+
+        self.rule = rule  # CFG Rule.
+        self.dot = dot  # Dot position in the rule.
+        self.sent_pos = sent_pos  # Sentence position.
+        self.chart_pos = chart_pos  # Chart index.
+        self.back_pointers = back_pointers  # Pointers to child states
+
+    def __eq__(self, other):
+        if type(other) is EarleyState:
+            return self.rule == other.rule and self.dot == other.dot and \
+                   self.sent_pos == other.sent_pos
+
+        return False
+
+    def __len__(self):
+        return len(self.rule)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        def str_helper(state):
+            return ('(' + state.rule.lhs + ' -> ' +
+                    ' '.join(state.rule.rhs[:state.dot] + ['*'] +
+                             state.rule.rhs[state.dot:]) +
+                    (', [%d, %d])' % (state.sent_pos, state.chart_pos)))
+
+        return (str_helper(self) +
+                ' (' + ', '.join(str_helper(s) for s in self.back_pointers) + ')')
+
+    def next(self):
+        """
+        Return next symbol to parse, i.e. the one after the dot
+        """
+
+        if self.dot < len(self):
+            return self.rule[self.dot]
+
+    def is_complete(self):
+        """
+        Checks whether the given state is complete.
+        """
+
+        return len(self) == self.dot
+
+    @staticmethod
+    def init():
+        """
+        Returns the state used to initialize the chart in the Earley algorithm.
+        """
+
+        return EarleyState(Rule(EarleyState.GAM, ['S']))
--- Labs/lab4/Earley/main.py
+++ Labs/lab4/Earley/main.py
@@ -6,6 +6,7 @@ def main():
     input_file = f"../{sys.argv[1]}" if len(sys.argv) > 1 else "../grammar.txt"
     grammar = Grammar.load_grammar(input_file)
 
+    print(grammar.rules)
     print(grammar)
 
 
